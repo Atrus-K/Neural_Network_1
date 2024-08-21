f7d5906715d7a6909da6206bea613856d2eab7cb
Atrus-K <147612159+Atrus-K@users.noreply.github.com>
2024-08-21 20:55:29 +0100

    Push 1
    Using git desktop to push, purely testing this to see if it is easier to use this. terminal or the commit and push funchtion in vscode

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ A brisk foray into the world of neural networks, using the MNIST data set to tea
 Simply out of pure boredom I'm aiming to develop a neural network as " from scratch" as possible. In an attempt to grasp how they work to a better extent (from an implemented point of view).
 
 Throughout this project I aim to use this README as an explaination, and a diary of sorts to jot down general processes, and things learnt whilst partaking in this adventure!
+
+This is mostly learning how they work
+
+I am aiming to build a neural network at the end of it that can read numbers, this will be using the MNIST data set.
+
+"""
+might want to look at 3blue1brown's series on neural networks. It's relatively math heavy but it really helped me to understand backpropagation more intuitively
+""""
+
+
+
---
+++ Singular_Neuron_Example.py
@@ -0,0 +1,23 @@
+
+#here we are coding a neuron, with the exmple of being fed the results of...
+# 3 other neurons( outputs from neurons themselves from a previous layer ), or the "input layer"
+inputs = [1,2,3,2.5]
+
+
+weights = [[0.2,0.8,-0.5,1],[0.5,-0.91,0.26,-0.5],[-0.26,-0.27,0.17,0.87]] #every neuron has weights 
+
+biases= [2,3,0.5] #every neuron has a bias ( list of biases )
+
+# first step for a neuron is to do inputs * weights + bias
+
+layer_outputs = [] #output of current layer - 
+for neuron_weights, neuron_bias in zip(weights,biases): # zip combines two lists into a list of lists element wise
+    neuron_output = 0 # output of given neuron 
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output +-n_input*weight
+    neuron_output =- neuron_bias
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
+
+
